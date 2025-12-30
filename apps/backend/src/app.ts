959399d5d80b0f3f121cd07ef4cdaee953170332
Steven Gourgy <stevengourgy@hotmail.com>
2025-12-29 20:42:14 -0500

    feat(#370)-Increase API rate limitting for development

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -20,7 +20,7 @@ export const createApp = () => {
   // Rate limiting
   const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: process.env.NODE_ENV === 'development' ? 1000 : 100,
+    max: 10000,
     standardHeaders: true,
     legacyHeaders: false,
     message: 'Too many requests from this IP, please try again later.',
